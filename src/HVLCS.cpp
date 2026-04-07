778ad5c5cefece5fe92868f2135b37f7b502af88
alex-chae-kim <alexk2k05@gmail.com>
2026-04-06 21:43:23 -0400

    Add dp implementation with backtracking

--- src/HVLCS.cpp
+++ src/HVLCS.cpp
@@ -17,12 +17,45 @@ int main() {
     string A, B;
     cin >> A >> B;
 
-    cout << "K: " << K << endl;
-    for (auto p : val) {
-        cout << "char: " << p.first << " val: " << p.second << endl;
+    int n = A.size(), m = B.size();
+
+    // dp[i][j] = max value of common subsequence of A[0..i-1] and B[0..j-1]
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (A[i-1] == B[j-1]) {
+                // Characters match: take it (add its value) or skip
+                int charVal = val.count(A[i-1]) ? val[A[i-1]] : 0;
+                dp[i][j] = max(dp[i-1][j-1] + charVal, max(dp[i-1][j], dp[i][j-1]));
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
     }
-    cout << "A: " << A << endl;
-    cout << "B: " << B << endl;
+
+    string result = "";
+    int i = n, j = m;
+    while (i > 0 && j > 0) {
+        if (A[i-1] == B[j-1]) {
+            int charVal = val.count(A[i-1]) ? val[A[i-1]] : 0;
+            if (dp[i][j] == dp[i-1][j-1] + charVal) {
+                result = A[i-1] + result;
+                i--; j--;
+            } else if (dp[i-1][j] >= dp[i][j-1]) {
+                i--;
+            } else {
+                j--;
+            }
+        } else if (dp[i-1][j] >= dp[i][j-1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    cout << dp[n][m] << "\n";
+    cout << result;
 
     return 0;
 }
